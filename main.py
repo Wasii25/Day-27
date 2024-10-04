01a7f6d8f64f74ac81ccf0137ab6f74b1a6a2959
Wasii25 <wasiullahrafeeq.s@gmail.com>
2024-10-04 15:24:33 +0530

    Commit

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from tkinter import Tk, Label, Button, Entry
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-#Label
-
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
-
 def button_clicked():
     print("I got clicked")
     new = input.get()
     my_label.config(text=new)
 
 
-button = Button(text="Click Me!", command=button_clicked)
-button.pack()
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
+#Label
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=100, pady=200)
+
+#Button
+button = Button(text="Click Me!", command=button_clicked)
+button.grid(column=1, row=1)
+
+#new button
+new_button = Button(text="Click Me!", command=button_clicked)
+new_button.grid(column=2, row=0)
+
+#Entry
 input = Entry()
-input.pack()
+input.grid(column=3, row=2)
 print(input.get())
 
 window.mainloop()
