254c8f309bf35a3f6a1db6cecc05a515c0ef6126
Wasii25 <wasiullahrafeeq.s@gmail.com>
2024-10-13 17:56:44 +0530

    Day 27

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
-from tkinter import Tk, Label, Button, Entry
+# from tkinter import Tk, Label, Button, Entry
+#
+# def button_clicked():
+#     print("I got clicked")
+#     new = input.get()
+#     my_label.config(text=new)
+#
+#
+# window = Tk()
+# window.title("My First GUI Program")
+# window.minsize(width=500, height=300)
+# window.config(padx=100, pady=200)
+#
+# #Label
+# my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+# my_label["text"] = "New Text"
+# my_label.config(text="New Text")
+# my_label.grid(column=0, row=0)
+# my_label.config(padx=100, pady=200)
+#
+# #Button
+# button = Button(text="Click Me!", command=button_clicked)
+# button.grid(column=1, row=1)
+#
+# #new button
+# new_button = Button(text="Click Me!", command=button_clicked)
+# new_button.grid(column=2, row=0)
+#
+# #Entry
+# input = Entry()
+# input.grid(column=3, row=2)
+# print(input.get())
+#
+# window.mainloop()
+from tkinter import *
 
-def button_clicked():
-    print("I got clicked")
-    new = input.get()
-    my_label.config(text=new)
-
+def converter():
+    miles = float(miles_input.get())
+    km = miles*1.609
+    kilometer_result_label.config(text=f"{km}")
 
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-window.config(padx=100, pady=200)
+window.title("Mile to KM converter")
+window.config(padx=20, pady=20)
 
-#Label
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-my_label.grid(column=0, row=0)
-my_label.config(padx=100, pady=200)
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
 
-#Button
-button = Button(text="Click Me!", command=button_clicked)
-button.grid(column=1, row=1)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
-#new button
-new_button = Button(text="Click Me!", command=button_clicked)
-new_button.grid(column=2, row=0)
 
-#Entry
-input = Entry()
-input.grid(column=3, row=2)
-print(input.get())
+is_equal_to = Label(text="Is equal to")
+is_equal_to.grid(column=0, row=1)
+
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+
+kilometer_label = Label(text="km")
+kilometer_label.grid(column=2, row=1)
+
+
+calculate_button = Button(text="Calculate", command=converter)
+miles_input.grid(column=1, row=2)
 
 window.mainloop()
